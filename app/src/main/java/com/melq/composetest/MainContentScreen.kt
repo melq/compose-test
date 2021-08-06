2e371940baf65b04ff3d3e9b81f74fb35d12a1f0
melq <tu2sice@yahoo.co.jp>
2021-08-06 19:06:48 +0900

    :construction: ボタンによる数字の操作を実装

--- app/src/main/java/com/melq/composetest/MainContentScreen.kt
+++ app/src/main/java/com/melq/composetest/MainContentScreen.kt
@@ -1,14 +1,14 @@
 package com.melq.composetest
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
-import androidx.compose.material.ButtonColors
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -30,16 +30,15 @@ fun MainContentScreen() {
 
 @Composable
 fun MainContent() {
-    Column(horizontalAlignment = Alignment.End) {
+    val ansNum = remember { mutableStateOf(0) }
 
-        @Composable fun buttonSpacer() { Spacer(modifier = Modifier.size(16.dp)) }
-        val numFontSize = 24.sp
+    Column(horizontalAlignment = Alignment.End) {
 
         Row(modifier = Modifier
             .height(IntrinsicSize.Min)
             .width(IntrinsicSize.Min),
             verticalAlignment = Alignment.CenterVertically) {
-            Text(text = "test",
+            Text(text = ansNum.value.toString(),
                 fontSize = 40.sp,
                 modifier = Modifier
                     .weight(2.1f)
@@ -48,12 +47,13 @@ fun MainContent() {
                     .padding(6.dp)
                     .wrapContentWidth(Alignment.End))
 
-            buttonSpacer()
+            ButtonSpacer()
 
-            Button(onClick = {},
+            Button(onClick = { ansNum.value /= 10},
                 modifier = Modifier
                     .weight(1f)
-                    .width(100.dp).height(72.dp),
+                    .width(100.dp)
+                    .height(72.dp),
                 colors = ButtonDefaults.textButtonColors(backgroundColor = Color(
                     131, 78, 78, 255))) {
                 Image(painter = painterResource(id = R.drawable.ic_baseline_keyboard_backspace_24), contentDescription = "backSpace")
@@ -62,42 +62,56 @@ fun MainContent() {
 
         Spacer(modifier = Modifier.size(24.dp))
 
-        Column(horizontalAlignment = Alignment.End) {
-            val numRow = 3
-            for (i in 0..6 step numRow) {
-                Row() {
-                    for (j in 1 until numRow + 1) {
-                        Button(onClick = {}, modifier = Modifier
+        NumPad() {
+            val newNum = ansNum.value * 10 + it
+            if (newNum in 0..99)
+                ansNum.value = newNum
+        }
+    }
+}
+
+@Composable
+fun NumPad(onClick: (Int) -> Unit) {
+    val numFontSize = 24.sp
+    Column(horizontalAlignment = Alignment.End) {
+        val numRow = 3
+        for (i in 0..6 step numRow) {
+            Row() {
+                for (j in 1 until numRow + 1) {
+                    val num = i + j
+                    Button(onClick = { onClick(num) },
+                        modifier = Modifier
                             .width(100.dp)
                             .height(72.dp)) {
-                            Text(text = (i + j).toString(), fontSize = numFontSize)
-                        }
-                        if (j != numRow) buttonSpacer()
+                        Text(text = num.toString(), fontSize = numFontSize)
                     }
+                    if (j != numRow) ButtonSpacer()
                 }
-                buttonSpacer()
             }
-            Row() {
-                Button(onClick = {}, modifier = Modifier
+            ButtonSpacer()
+        }
+        Row() {
+            Button(onClick = { onClick(0) }, modifier = Modifier
+                .width(100.dp)
+                .height(72.dp)) {
+                Text(text = "0", fontSize = numFontSize)
+            }
+            ButtonSpacer()
+            Button(onClick = { onClick(100) },
+                modifier = Modifier
                     .width(100.dp)
-                    .height(72.dp)) {
-                    Text(text = "0", fontSize = numFontSize)
-                }
-                buttonSpacer()
-                Button(onClick = {},
-                    modifier = Modifier
-                        .width(100.dp)
-                        .height(72.dp),
-                    colors = ButtonDefaults.textButtonColors(backgroundColor = Color(
-                        82,
-                        129,
-                        83,
-                        255
-                    ), contentColor = Color.White
-                    )) {
-                    Text(text = "OK", fontSize = numFontSize)
-                }
+                    .height(72.dp),
+                colors = ButtonDefaults.textButtonColors(backgroundColor = Color(
+                    82,
+                    129,
+                    83,
+                    255
+                ), contentColor = Color.White
+                )) {
+                Text(text = "OK", fontSize = numFontSize)
             }
         }
     }
 }
+
+@Composable fun ButtonSpacer() { Spacer(modifier = Modifier.size(16.dp)) }
